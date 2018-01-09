a95a22e3ce48c7dc57d0d3fc3012e77eb8a708be
parkjieun <parkjieun24@gmail.com>
2018-01-09 13:32:20 +0900

    Go Project

---
+++ Go_project/src/main/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+)
+
+func temp() {
+	
+	cmd := "ver"
+	fmt.Println("command: ", cmd)
+	
+	out, err := exec.Command("cmd","/C",cmd).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	fmt.Println("result: ", string(out))
+
+}
+
+func db() {
+	 db, err := sql.Open("mysql", "hduser:tkdydwk00@tcp(1.234.25.136:3306)/mecab")
+       if err != nil {
+               log.Fatal(err)
+       }
+       defer db.Close()
+
+       var term string
+
+       rows, err := db.Query("SELECT pos FROM CoinedWord")
+       if err != nil {
+                log.Fatal(err)
+       }
+       
+       defer rows.Close() //반드시 닫아야 함
+
+       for rows.Next() {
+               err := rows.Scan(&term)
+               
+               if err != nil {
+               log.Fatal(err)
+               }
+               
+               fmt.Println(term)
+
+       }
+}
+
+func main() {
+	
+	db()
+	temp()
+	
+}
